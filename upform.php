21f4b4d48f19cf1f002ccb30a270d0f90b47caa1
zoltan.koppanyi <zoltan.koppanyi@9cca383b-f0be-8777-ac55-a46735f88ddb>
2012-12-17 13:36:39 +0000

    Remaining files have been commited!

--- upform.php
+++ upform.php
@@ -3,12 +3,17 @@
 
 		include_once("FormAPI/import.php");
 		include_once("config.php");
+
+		error_reporting(E_ALL);
 		
 		// Display results on client side
 		$model = new EchoModel();
 
 		// Create control object
 		$formapi = FormAPIFactory::getInstance()->create("sample.xml", "upform.php", $model);
+
+		//Handle bad request with an anonymous function
+		$formapi->setInvalidRequestHandler(function($f) { echo "Invalid value: " . $f->getName(); });
 
 		// Process request
 	    	$w = $formapi->request($_REQUEST);
