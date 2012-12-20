87e29a706f772a0e88c06d0168b3f41b574cb5c0
zoltan.koppanyi <zoltan.koppanyi@9cca383b-f0be-8777-ac55-a46735f88ddb>
2012-12-20 13:08:19 +0000

    Replace anonaymous functions

--- upform.php
+++ upform.php
@@ -13,7 +13,7 @@
 		$formapi = FormAPIFactory::getInstance()->create("sample.xml", "upform.php", $model);
 
 		//Handle bad request with an anonymous function
-		$formapi->setInvalidRequestHandler(function($f) { echo "Invalid value: " . $f->getName(); });
+		$formapi->setInvalidRequestHandler(request_error_callback);
 
 		// Process request
 	    	$w = $formapi->request($_REQUEST);
@@ -23,5 +23,9 @@
 
 	} catch (Exception $e) {
 	    echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+
+	function request_error_callback($f) {
+		echo "Invalid value: " . $f->getName();
 	}
 ?>
