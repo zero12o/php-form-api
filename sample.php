977a6645acbad1299a77307d02dd1b0cfb09bcd2
siki1958 <siki1958@gmail.com>
2013-02-10 12:02:35 +0000

    csv modell bug fixing, field with ";" or \n

--- sample.php
+++ sample.php
@@ -13,10 +13,11 @@
 	$model = new EchoModel();
 	$formapi = FormAPIFactory::getInstance()->create("sample.xml", $_SERVER['PHP_SELF'], $model);
 
-	// TODO inprove condition more specific to form
+	// TODO improve condition more specific to form
 	if (is_array($_REQUEST) && count($_REQUEST) > 2) {
+		$formapi->setInvalidRequestHandler("request_error_callback");
 		// response for submit
-		$w = $formapi->getModel()->process($_REQUEST, $formapi->getForm());
+		$w = $formapi->request($_REQUEST);
 		echo $w;
 	} else {
 		// generate form
